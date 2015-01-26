102e1f67697d64422711ffd98704c963a3904649
Jeromy Evans (personal) <jeromy.evans@blueskyminds.com.au>
2015-01-26 21:40:58 +1100

    Added contact detection between pacman and ghost and paceman and exit. Set collision bit masks and contact bit masks. Added scene as contactDelegate for physicsWorld

--- maze-demo-spritekit/maze-demo-spritekit/GameScene.m
+++ maze-demo-spritekit/maze-demo-spritekit/GameScene.m
@@ -8,7 +8,7 @@
 
 #import "GameScene.h"
 
-@interface GameScene ()
+@interface GameScene () <SKPhysicsContactDelegate>
 
 @property BOOL contentCreated;
 
@@ -23,6 +23,16 @@
 @end
 
 @implementation GameScene
+
+/**
+ These masks are used to categorise different kinds of contacts/collisions in the scene
+ */
+const uint32_t GHOST_COLLISION_MASK =   0x01;
+const uint32_t EXIT_COLLISION_MASK =    0x02;
+const uint32_t WALL_COLLISION_MASK =    0x04;
+const uint32_t PACMAN_COLLISION_MASK =  0x08;
+const uint32_t EDGE_COLLISION_MASK =    0x10;
+
 
 -(void)didMoveToView:(SKView *)view {
     
@@ -46,6 +56,12 @@
 
     // setup an edge loop (boundary)
     self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:skView.bounds];
+    self.physicsBody.categoryBitMask = EDGE_COLLISION_MASK;
+    self.physicsBody.collisionBitMask = PACMAN_COLLISION_MASK | GHOST_COLLISION_MASK;
+    self.physicsBody.contactTestBitMask = 0;
+    
+    // assign this class as the contact delegate so it's notified on contact/collision
+    self.physicsWorld.contactDelegate = self;
     
 //    [self createLabel:skView];
     
@@ -119,6 +135,10 @@
     physicsBody.affectedByGravity = false;
     physicsBody.mass = 0.2;
     
+    physicsBody.categoryBitMask = PACMAN_COLLISION_MASK;
+    physicsBody.collisionBitMask = EXIT_COLLISION_MASK | GHOST_COLLISION_MASK | WALL_COLLISION_MASK | EDGE_COLLISION_MASK;
+    physicsBody.contactTestBitMask = EXIT_COLLISION_MASK | GHOST_COLLISION_MASK;
+    
     self.pacmanSprite.physicsBody = physicsBody;
 }
 
@@ -151,6 +171,8 @@
         ghostSprite.position = CGPointMake(position.x, position.y+ghostSprite.size.height/2);
     }
     
+    // add animation for the ghost.
+
     CGFloat animationHeight = self.view.bounds.size.height - ghostSprite.size.height;
     
     SKAction *moveDown = [SKAction moveByX:0 y:-animationHeight duration: 2];
@@ -168,14 +190,17 @@
     
     SKAction *repeatedSequence = [SKAction repeatActionForever:sequence];
     
+    // enable physics for the ghost.
     
     SKPhysicsBody* physicsBody = [SKPhysicsBody bodyWithRectangleOfSize: ghostSprite.frame.size];
     physicsBody.dynamic = true;
     physicsBody.affectedByGravity = false;
     physicsBody.mass = 0.2;
+    physicsBody.categoryBitMask = GHOST_COLLISION_MASK;
+    physicsBody.collisionBitMask = PACMAN_COLLISION_MASK | WALL_COLLISION_MASK | EDGE_COLLISION_MASK;
+    physicsBody.contactTestBitMask = PACMAN_COLLISION_MASK;
     
     ghostSprite.physicsBody = physicsBody;
-    
     
     [ghostSprite runAction: repeatedSequence];
     
@@ -203,6 +228,71 @@
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+}
+
+
+- (void)didBeginContact: (SKPhysicsContact *)contact {
+    
+    uint32_t bitMaskA = contact.bodyA.categoryBitMask;
+    uint32_t bitMaskB = contact.bodyB.categoryBitMask;
+    
+    // check the type of collision
+    if (((bitMaskA & PACMAN_COLLISION_MASK) != 0 && (bitMaskB & GHOST_COLLISION_MASK)) != 0 ||
+        ((bitMaskB & PACMAN_COLLISION_MASK) != 0 && (bitMaskA & GHOST_COLLISION_MASK)) != 0)
+    {
+        // pacman vs ghost
+        SKNode *ghost;
+        if ([contact.bodyA.node.name rangeOfString:@"ghost"].location == NSNotFound)
+        {
+            ghost = contact.bodyB.node;
+        }
+        else {
+            ghost = contact.bodyA.node;
+        }
+        
+        [self flashGhost:(SKSpriteNode*) ghost];
+    }
+    else if (((bitMaskA & PACMAN_COLLISION_MASK) != 0 && (bitMaskB & EXIT_COLLISION_MASK)) != 0 ||
+             ((bitMaskB & PACMAN_COLLISION_MASK) != 0 && (bitMaskA & EXIT_COLLISION_MASK)) != 0)
+         {
+             // pacman vs exit
+             SKNode *exitSprite;
+             if ([contact.bodyA.node.name isEqualToString: @"exit"])
+             {
+                 exitSprite = contact.bodyA.node;
+             }
+             else {
+                 exitSprite = contact.bodyB.node;
+             }
+             
+             [self flashExit:(SKSpriteNode*) exitSprite];
+            
+         }
+
+}
+
+-(void)flashGhost:(SKSpriteNode*) ghost
+{
+    SKAction *fadeOut = [SKAction fadeOutWithDuration:0.1];
+    SKAction *fadeIn= [SKAction fadeInWithDuration:0.1];
+    
+    SKAction *sequence = [SKAction sequence: @[fadeOut, fadeIn]];
+    
+    SKAction *repeatedSequence = [SKAction repeatAction:sequence count:3];
+    
+    [ghost runAction:repeatedSequence];
+}
+
+-(void)flashExit:(SKSpriteNode*) exitSprite
+{
+    SKAction *fadeOut = [SKAction fadeOutWithDuration:0.5];
+    SKAction *fadeIn= [SKAction fadeInWithDuration:0.5];
+    
+    SKAction *sequence = [SKAction sequence: @[fadeOut, fadeIn]];
+    
+    SKAction *repeatedSequence = [SKAction repeatAction:sequence count:3];
+    
+    [exitSprite runAction:repeatedSequence];
 }
 
 @end
